a8f9ae8f999cec3f24c754528afc173e3bd9cda2
Alec Lazarescu <Alec.Lazarescu@pearson.com>
2014-03-25 14:44:21 -0400

    Small windows/vagrant changes
    - fixed .sh file line terminators
    - added batch file to run putty to connect to vagrant run VM
    - added Vagrantfile override to stamm/precise64-docker with port
    forwards for our sample

---
+++ vagrant-misc/vssh.bat
@@ -0,0 +1,3 @@
+@REM This is the default for your local vagrant so not a big secret
+@REM Alternative is to use puttygen to make a .ppk file for SSH
+@start putty -ssh -P 2222 -l vagrant -pw vagrant localhost
